f129073ed6439d01d510f28cfda91bfd8f90ac33
Kevin Lam <kevnivek303@yahoo.com>
2023-07-18 17:26:38 -0400

    update

---
+++ e-commerce/client/src/pages/Update.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Update = () => {
+  const [car, setCar] = useState({
+    name: '',
+    description: '',
+    price: '',
+  });
+// To refresh page and nagivate to targeted page
+  const navigate = useNavigate();
+// To target element 
+  const location = useLocation();
+
+const carId = location.pathname.split('/')[2];
+  console.log(location.pathname.split('/')[2])
+
+  const handleChange = (e) => {
+    setCar((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      navigate('/product');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(car);
+  return (
+    <div className='form'>
+      <h1>Update the car</h1>
+      <input type='text' placeholder='name' onChange={handleChange} name='name' />
+      <input type='text' placeholder='description' onChange={handleChange} name='description' />
+      <input type='number' placeholder='price' onChange={handleChange} name='price' />
+      <button className='formButton' onClick={handleClick}>Update Car</button>
+    </div>
+  );
+};
+
+export default Update;
+
+
+
+
+
