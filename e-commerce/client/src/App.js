bc1bb839a6be883b8bec99726a0db5ee57c9a8b9
Kevin Lam <kevnivek303@yahoo.com>
2023-07-18 17:27:30 -0400

    added app

--- e-commerce/client/src/App.js
+++ e-commerce/client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Car from './pages/Car';
+import Add from './pages/Add';
+import Update from './pages/Update';
+import Navbar from './navandfooter/Navbar';
+import Footer from './navandfooter/Footer';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product" element={<Car />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update/:id" element={<Update />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
